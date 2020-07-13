1255cfee09d0fc752691f80689099ee33238eb67
yulibumeng <820729033@qq.com>
2020-07-13 23:22:00 +0800

    buzaxing

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,18 +1,15 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
+  <div>
+      登录组件
   </div>
 </template>
 
 <script>
-
-
 export default {
-  name: 'app',
-  
 }
 </script>
 
-<style>
+<style scoped>
 
 </style>
+
