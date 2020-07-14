af5bde3958711468dd5306ea4a643f5bb8a764c8
yulibumeng <820729033@qq.com>
2020-07-14 22:43:07 +0800

    完成了登录功能

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,13 @@
 import Vue from 'vue'
 import { Button } from 'element-ui'
-
+import { Form,FormItem } from 'element-ui'
+import { Input } from 'element-ui'
+//导入弹框提示 （需要全局挂载）
+import { Message } from 'element-ui'
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+//全局挂载
+Vue.prototype.$message=Message;
+
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,27 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Login from './components/Login.vue'
-
+import Login from '../components/Login.vue'//这里记得使用../使用./找不到路径
+import Home from '../components/Home.vue'
 Vue.use(VueRouter)
 
   
-export default new VueRouter({
+const router= new VueRouter({
   routes:[
-    {path:'/login', component: Login}
+    {path:'/', redirect: '/login'},
+    {path:'/login', component: Login},
+    {path:'/home', component: Home}
   ]
 })
-
+//挂载路由导航守卫
+router.beforeEach((to ,from , next) => {
+   //to表示将要访问的路径
+   //from表示从哪个路径来的
+   //next一个函数，表示放行
+   //next()放行 next('/login')强制跳转
+   if(to.path==='/login') return next()
+   //获取tkoen
+   const tokenstr= window.sessionStorage.getItem('token')
+   if(!tokenstr) return next('/login')
+   next()
+})
+export default router
