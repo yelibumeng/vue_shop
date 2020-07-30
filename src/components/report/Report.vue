035db9f54b28b6d8fe0a3108864f7070129ead35
yulibumeng <820729033@qq.com>
2020-07-30 22:57:34 +0800

    完成数据页面

---
+++ src/components/report/Report.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>  
+      <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>数据统计</el-breadcrumb-item>
+      <el-breadcrumb-item>数据报表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-card>
+    <div id="main"  style="width: 600px;height:400px;"></div>
+    </el-card>
+    </div>
+</template>
+
+<script>
+import echarts from 'echarts'
+import _ from 'lodash'
+export default {
+data(){
+    return{
+options: {
+        title: {
+          text: '用户来源'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross',
+            label: {
+              backgroundColor: '#E9EEF3'
+            }
+          }
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: [
+          {
+            boundaryGap: false
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value'
+          }
+        ]
+      }
+    }
+},
+created(){
+
+},
+async mounted(){
+  var myChart = echarts.init(document.getElementById('main'));
+   const { data: res } = await this.$http.get('reports/type/1');
+
+      if (res.meta.status != 200) {
+        return this.$message.error("数据报表获取失败");
+      }
+     this.$message.success("数据报表获取成功");
+  const result= _.merge(res.data,this.options)
+
+        // 使用刚指定的配置项和数据显示图表。
+        myChart.setOption(result);
+},
+methods:{
+
+},
+}
+</script>
+
+<style lang="less"  scoped>
+
+</style>
